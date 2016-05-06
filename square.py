146d77fa5c19f033485841b68d7292c15869f892
Amanda McCumber <Serenity333@users.noreply.github.com>
2016-05-06 13:53:19 -0400

    Update square.py

--- square.py
+++ square.py
@@ -1,6 +1,6 @@
 #module to draw a square with a Turtle. WARNING: Does NOT include the Turtle setup
 
-def square(t):
+def square(t, length):
     for i in range(4):
-        t.fd(100)
+        t.fd(length)
         t.lt(90)
